d3e527fc78f56d99503677c8e79578b488865311
IffatAfsara <113348886+IffatAfsara@users.noreply.github.com>
2023-09-17 17:13:03 +0600

    Update bussiness_assesment.py

--- bussiness_assesment.py
+++ bussiness_assesment.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1YAm256Ok5vhQOmUZ2dow_rRC3BLYwmSR
 """
 
-!pip install pandas streamlit
+#!pip install pandas streamlit
 
 from google.colab import files
 uploaded = files.upload()
